00b0c933781e61d552ab0402afccdad6cec85182
FernandoHen <11hengsteber@gmail.com>
2018-02-20 05:01:35 -0600

    Movimiento y enemigo

---
+++ Proyecto/Assets/Scripts/Fernando.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fernando: MonoBehaviour
+{
+
+    Rigidbody2D rb2d;
+    SpriteRenderer sr;
+    public Camera cam;
+    private float speed = 5f;
+    private float jumpForce = 230f;
+    private bool facingRight = true;
+    Animator anim;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        if (move != 0)
+        {
+            rb2d.transform.Translate(new Vector3(1, 0, 0) * move * speed * Time.deltaTime);
+            cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+            facingRight = move > 0;
+        }
+
+        sr.flipX = !facingRight;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(Vector2.up * jumpForce);
+
+            anim.SetFloat("Speed", Mathf.Abs(move));
+
+        }
+       
+
+    }
+}
