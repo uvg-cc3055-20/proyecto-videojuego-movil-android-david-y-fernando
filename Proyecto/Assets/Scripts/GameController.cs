8add284dd9090ab7ca7d342f8aac3a7edb7cc397
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-02-27 23:37:47 -0600

    Revert "Sonido"
    This reverts commit 82abef0f024c5b1d117d5c89e6994e9ad08f0462.

--- Proyecto/Assets/Scripts/GameController.cs
+++ Proyecto/Assets/Scripts/GameController.cs
@@ -8,10 +8,9 @@ public class GameController : MonoBehaviour {
     public float score;
     public static GameController instance;
 
-    //public AudioSource musicPlayer;
     // Use this for initialization
     void Start () {
-       // musicPlayer = GetComponent<AudioSource>();
+
         gameOver = false;
         score = 0;
         instance = this;
@@ -20,7 +19,6 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //musicPlayer.Play();
 		
 	}
 }
