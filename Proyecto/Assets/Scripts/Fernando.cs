ea018a2250847434d69ea919ad8facacef7729c6
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-02-28 00:11:04 -0600

    .

--- Proyecto/Assets/Scripts/Fernando.cs
+++ Proyecto/Assets/Scripts/Fernando.cs
@@ -39,8 +39,8 @@ public class Fernando : MonoBehaviour
     {
         if (GameController.instance.gameOver == false)
         {
-            float move = CrossPlatformInputManager.GetAxis("Horizontal");
-            //float move = Input.GetAxis("Horizontal");
+           // float move = CrossPlatformInputManager.GetAxis("Horizontal");
+            float move = Input.GetAxis("Horizontal");
             if (move != 0)
             {
                 rb2d.transform.Translate(new Vector3(1, 0, 0) * move * speed * Time.deltaTime);
@@ -52,8 +52,8 @@ public class Fernando : MonoBehaviour
             sr.flipX = !facingRight;
 
 
-            //if (Input.GetButtonDown("Jump"))
-            if (CrossPlatformInputManager.GetButtonDown("Jump"))
+            if (Input.GetButtonDown("Jump"))
+            //if (CrossPlatformInputManager.GetButtonDown("Jump"))
             {
                 RaycastHit2D raycast = Physics2D.Raycast(feet.transform.position, Vector2.down, 1f, layerMask);
                 Debug.Log(raycast.collider);
